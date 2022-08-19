ff1751e9ee58e7666f67057fb22fa03d40576cfb
weldson <weldson.amaral@gmail.com>
2022-08-19 10:33:14 -0300

    feat: add eslint and prittier plugins and settings

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response} from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
