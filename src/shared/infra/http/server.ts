6c5aad6c6278c4b052bea47a7e0e461f1251ca1c
weldson <weldson.amaral@gmail.com>
2022-08-19 08:33:08 -0300

    first commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,12 @@
+import express, { NextFunction, Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log('Server started at port 3333');
+});
